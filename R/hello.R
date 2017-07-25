81b96cc3ab993f7009b245f4abee61ed11943f79
Neal Fultz <nfultz@gmail.com>
2017-07-25 00:10:58 +0000

    Make url_splice friendly.

--- R/hello.R
+++ R/hello.R
@@ -12,7 +12,23 @@ NULL
 `[<-.naughty` <- function(x,i=NULL,j=NULL, value) .Call("vmatrix_set", x, i, j, value, PACKAGE='naughtyurl')
 
 #' @export
-url_splice <- function(x, j, v) .Call("do_splice", x, j, v, PACKAGE='naughtyurl')
+url_splice <- function(x, scheme, user, password, host, port, path, query, fragment){
+  cols = integer(0)
+  v = list()
+
+  if(!missing(scheme))   {cols <- c(cols, 0x10L); v[['scheme']]<- scheme}
+  if(!missing(user))     {cols <- c(cols, 0x11L); v[['user']]<- user}
+  if(!missing(password)) {cols <- c(cols, 0x12L); v[['password']]<- password}
+  if(!missing(host))     {cols <- c(cols, 0x13L); v[['host']]<- host}
+  if(!missing(port))     {cols <- c(cols, 0x14L); v[['port']]<- port}
+  if(!missing(path))     {cols <- c(cols, 0x15L); v[['path']]<- path}
+  if(!missing(query))    {cols <- c(cols, 0x16L); v[['query']]<- query}
+  if(!missing(fragment)) {cols <- c(cols, 0x17L); v[['fragment']]<- fragment}
+
+  if(length(cols) == 0) return(x)
+
+  .Call("do_splice", x, cols, v, PACKAGE='naughtyurl')
+}
 
 #' @export
 naughty <- function(x) UseMethod("naughty")
